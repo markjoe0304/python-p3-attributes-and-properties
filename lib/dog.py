4473b4ed22896057cd86d1c5668a2b5aac2350d2
markjoe0304 <markjoegicheru@gmail.com>
2024-09-04 21:47:41 +0300

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,11 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Mastiff"):
+        self.name = name
+        self.breed = breed  
+
+        if not isinstance(name, str) or len(name) < 1 or len(name) > 25:
+            print("Name must be string between 1 and 25 characters.")
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
